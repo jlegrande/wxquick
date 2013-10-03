95650d2bd11d39764e362bee69a56f00e8bad5ca
John LeGrande <johnl@floathybrid.com>
2013-10-02 19:34:34 -0700

    Add basic support for menus

--- __init__.py
+++ __init__.py
@@ -31,7 +31,21 @@ def sizer_packer(container, parent):
         child.pack(parent)
         container.Add(child, child.proportion, child.flag, child.border)
 
-        
+def menu_packer(menu, frame):
+    menu.name = menu._kwargs.get('name', '')
+    menu.wxClass.__init__(menu)
+    for item_id, item_text, item_help, callback in menu.children:
+        menuitem = menu.Append(item_id, item_text, item_help)
+        frame.Bind(wx.EVT_MENU, callback, menuitem)
+
+def menubar_packer(menubar, parent):
+    parent.CreateStatusBar()
+    menubar.wxClass.__init__(menubar)
+    for menu in menubar.children:
+        menu.pack(parent)
+        menubar.Append(menu, menu.name)
+
+    parent.SetMenuBar(menubar)
 
         
 # Wrapper Classes
@@ -39,6 +53,8 @@ def sizer_packer(container, parent):
 class WxFrame(WxContainer, wx.Frame): packer = frame_packer
 class WxStaticText(WxWidget, wx.StaticText): pass
 class WxListBox(WxWidget, wx.ListBox): events = [event.listbox]
+class WxMenu(WxContainer,wx.Menu): packer = menu_packer
+class WxMenuBar(WxContainer, wx.MenuBar): packer = menubar_packer
 class WxTextCtrl(WxWidget, wx.TextCtrl): pass
 class SizerPanel(WxContainer, wx.Panel): packer = sizer_layout_packer
 class VBox(WxContainer, containers.VerticalBox): packer = sizer_packer
